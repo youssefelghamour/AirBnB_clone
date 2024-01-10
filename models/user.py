a50a31caa5303745d6651630967fdc287e55b50e
youssef <youssefelghamour@gmail.com>
2024-01-10 01:37:05 -0800

    add user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" User class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ class User that inherits from BaseModel """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
