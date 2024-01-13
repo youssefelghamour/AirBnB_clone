4b5fcd75b1ae4d5560f07c4a08ae0a4f629aa839
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-13 03:17:29 -0800

    Tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,4 +10,14 @@ class TestFileStorage(unittest.TestCase):
     def test_all(self):
         '''Test all method of FileStorage'''
         storage = FileStorage()
+        self.assertEqual(storage.all(), {})
         self.assertIsInstance(storage.all(), dict)
+
+    def test_new(self):
+        '''test new method of FileStorage'''
+        new_ins = BaseModel()
+        storage = FileStorage()
+        storage.new(new_ins)
+        self.assertNotEqual(storage.all(), {})
+        key = new_ins.__class__.__name__ + "." + new_ins.id
+        self.assertTrue(key in storage.all())
