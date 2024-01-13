f4ef72d504fac0cffe1bcd5617efa72e73ced477
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-13 03:22:18 -0800

    Tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,17 +7,21 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     '''Test class for FileStorage'''
+    def setUp(self):
+        '''class initializations'''
+        storage = FileStorage()
+        new_ins = BaseModel()
+
     def test_all(self):
         '''Test all method of FileStorage'''
-        storage = FileStorage()
         self.assertEqual(storage.all(), {})
         self.assertIsInstance(storage.all(), dict)
 
     def test_new(self):
         '''test new method of FileStorage'''
-        new_ins = BaseModel()
-        storage = FileStorage()
         storage.new(new_ins)
         self.assertNotEqual(storage.all(), {})
         key = new_ins.__class__.__name__ + "." + new_ins.id
         self.assertTrue(key in storage.all())
+
+    def test_save(self)
