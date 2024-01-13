6d5c395a238c4ea2301a554e0bcabf858568a53c
youssef <youssefelghamour@gmail.com>
2024-01-13 04:27:33 -0800

    add more test cases to tests/test_models/test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -28,6 +28,36 @@ class TestBaseModel(unittest.TestCase):
         model = BaseModel(name="first_model")
         self.assertEqual(model.name, "first_model")
 
+    def test_init_created_at(self):
+        """ Tests __init__ with a custom updated_at values """
+        date = datetime(2024, 1, 13, 11, 58)
+        date_str = str(date)
+        model = BaseModel(created_at=date_str)
+        self.assertEqual(model.created_at, date)
+
+    def test_init_updated_at(self):
+        """ Tests __init__ with a custom updated_at values """
+        date = datetime(2024, 1, 13, 11, 58)
+        date_str = str(date)
+        model = BaseModel(updated_at=date_str)
+        self.assertEqual(model.updated_at, date)
+
+    def test_init_without_arguments(self):
+        """ Tests __init__ without any arguments """
+        my_model = BaseModel()
+
+        # Checks if the id is generated
+        self.assertTrue(hasattr(my_model, 'id'))
+        self.assertIsInstance(my_model.id, str)
+        self.assertNotEqual(my_model.id, "")
+
+        # Checks if created_at and updated_at are generated
+        self.assertTrue(hasattr(my_model, 'created_at'))
+        self.assertTrue(isinstance(my_model.created_at, datetime))
+
+        self.assertTrue(hasattr(my_model, 'updated_at'))
+        self.assertTrue(isinstance(my_model.updated_at, datetime))
+
     def test_str(self):
         """ Tests the string representation of the model with
             custom instances attributes """
@@ -44,7 +74,11 @@ class TestBaseModel(unittest.TestCase):
     def test_to_dict(self):
         """ Tests the dictionary representation of the model """
         model = BaseModel()
-        self.assertEqual(model.to_dict(), model.__dict__)
+        expected_dict = model.__dict__.copy()
+        expected_dict['created_at'] = expected_dict['created_at'].isoformat()
+        expected_dict['updated_at'] = expected_dict['updated_at'].isoformat()
+        expected_dict['__class__'] = model.__class__.__name__
+        self.assertEqual(model.to_dict(), expected_dict)
 
     def test_to_dict2(self):
         """ Tests the dictionary representation of the model
@@ -53,4 +87,18 @@ class TestBaseModel(unittest.TestCase):
         date_str = str(date)
         model = BaseModel(number=15, name="test_model",
                           created_at=date_str, updated_at=date_str)
-        self.assertEqual(model.to_dict(), model.__dict__)
+        expected_dict = model.__dict__.copy()
+        expected_dict['created_at'] = expected_dict['created_at'].isoformat()
+        expected_dict['updated_at'] = expected_dict['updated_at'].isoformat()
+        expected_dict['__class__'] = model.__class__.__name__
+        self.assertEqual(model.to_dict(), expected_dict)
+
+    def test_save(self):
+        """ Tests if save method correctly updates updated_at """
+        import time
+        my_model = BaseModel()
+        initial_updated_at = my_model.updated_at
+        time.sleep(1)
+        my_model.save()
+        self.assertNotEqual(initial_updated_at, my_model.updated_at)
+        self.assertTrue(initial_updated_at < my_model.updated_at)
