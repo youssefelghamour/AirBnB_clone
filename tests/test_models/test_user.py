089b6df5c88649c577606cf73dd40d3b041ed089
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-13 10:52:07 -0800

    Test User class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 '''Test module for User class'''
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -12,3 +13,9 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(User, "password"))
         self.assertTrue(hasattr(User, "first_name"))
         self.assertTrue(hasattr(User, "last_name"))
+
+    def test_User_instance(self):
+        '''test User class with an instance'''
+        ins = User()
+        self.assertIsInstance(ins, User)
+        self.assertTrue(issubclass(type(ins), BaseModel))
