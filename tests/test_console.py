4b766d75b6a9662d436c9f6d7fa575d9195d6776
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-14 01:07:23 -0800

    Console unittests

--- tests/test_console.py
+++ tests/test_console.py
@@ -28,6 +28,11 @@ EOF  all  count  create  destroy  help  quit  show  update
 
 '''
         self.assertEqual(help_str, f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:                                                                                     HBNBCommand().onecmd("help quit")
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
         help_quit = 'Quit command to exit the program\n'
+        self.assertEqual(help_quit, f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+        help_quit = 'EOF exit to the program\n'
         self.assertEqual(help_quit, f.getvalue())
