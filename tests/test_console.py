87af19b7060772d0c17b307d6bd0d4d153c95561
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-01-14 00:29:36 -0800

    Console unittests

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Test module for console'''
+import unittest
+from console import HBNBCommand
+import sys
+from io import StringIO
+from unittest.mock import patch
+
+
+class TestConsole(unittest.TestCase):
+    '''Test class for console module'''
+    def test_quit_cmd(self):
+        '''test quit command of console'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
